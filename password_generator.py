63ff51ac7d52f37d0d46bf584a324c4ae5047cac
Sidharth674 <sidharthsinghrajput674@gmail.com>
2026-04-27 15:48:23 +0530

    Improved password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,12 +1,51 @@
 import random
 import string
+import os
 
-vals = (string.ascii_letters + string.punctuation + string.digits)
+if not os.path.exists("password.txt"):
+    with open("password.txt" , "w") as f:
+        pass
+
+val = (string.ascii_letters + string.punctuation + string.digits)
 
 password = ("")
-pass_len = int(input("Enter the length of password you want to get:  "))
+while True:
+    try:
+        pass_len = int(input("Enter the length of password you need: "))
+        break
+    except ValueError:
+        print("Invalid input")
+    
 
 for i in range(pass_len):
-    password += (random.choice(vals))
-        
-print("Your password is" , password)
+    password += (random.choice(val))
+    
+print("Your password is " , password)
+
+while True:
+    user = input("What do you want to do ?[exit(e) , get another password(g) , save password(s) , get passwords(p) , clear your password file(c)]: ").lower()
+    
+    if(user == "e"):
+        break
+    elif(user == "g"):
+        try:
+            password = ("")
+            pass_len2 =int(input("Enter the length of password you need: "))
+            for i in range(pass_len2):
+             password += (random.choice(val))
+    
+            print("Your password is " , password)
+        except ValueError:
+            print("Invalid input")
+            
+    elif(user == "s"):
+        with open("password.txt" , "a") as f:
+            f.write(password + "\n")
+            print("Saved!")
+    elif(user == "p"):
+        with open("password.txt" , "r")as f:
+            print(f.read())
+
+    elif(user == "c"):
+        with open("password.txt" , "w")as f:
+            pass
